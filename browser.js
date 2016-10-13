88de5969feeb42db37c8c9f6bbe9c67082a58c64
Sergey Lysenko <soulwish.ls@gmail.com>
2016-10-13 11:40:02 +0300

    add app shortcuts

--- browser.js
+++ browser.js
@@ -1,1 +1,41 @@
 window.__devtron = {require: require, process: process}
+const Mousetrap = require('mousetrap');
+const $ = document.querySelector.bind(document);
+
+function registerShortcuts() {
+    Mousetrap.bind('g h', () => {
+        $('a[href^="/home.php"]').click();
+    });
+
+    Mousetrap.bind('g f', () => {
+        $('a[href^="/friends"]').click();
+    });
+
+    Mousetrap.bind('g m', () => {
+        $('a[href^="/messages"]').click();
+    });
+
+    Mousetrap.bind('g n', () => {
+        $('a[href^="/notifications.php"]').click();
+    });
+
+    Mousetrap.bind('g s', () => {
+        $('a[href^="/search/"]').click();
+    });
+
+    Mousetrap.bind('g p', () => {
+        $('#bookmarks_jewel a').click();
+    });
+
+    Mousetrap.bind(['backspace', 'left'], () => {
+		window.history.back();
+	});
+
+    Mousetrap.bind('right', () => {
+		window.history.forward();
+	});
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	registerShortcuts();
+});
